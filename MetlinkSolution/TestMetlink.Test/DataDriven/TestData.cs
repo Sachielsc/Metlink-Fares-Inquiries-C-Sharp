81c44a1ab427405b5315bb28449d710032050531
Charles Shu <cshu@planittesting.com>
2018-06-19 10:51:11 +1200

    1. update all the extensions 2. add data-driven file path to app.config 3. add the class TestData

---
+++ MetlinkSolution/TestMetlink.Test/DataDriven/TestData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Objectivity.Test.Automation.Tests.NUnit.DataDriven;
+using System.Configuration;
+
+namespace TestMetlink.Test.DataDriven
+{
+	/// <summary>
+	/// DataDriven methods for NUnit test framework
+	/// </summary>
+	public static class TestData
+	{
+		public static IEnumerable CredentialsExcel
+		{
+			get
+			{
+				/// <remarks>
+				/// call the method ReadXlsxDataDriveFile() from its definitions:
+				/// public static IEnumerable<TestCaseData> ReadXlsxDataDriveFile(string path, string sheetName, [Optional] string[] diffParam, [Optional] string testName)
+				/// </remarks>
+				return DataDrivenHelper.ReadXlsxDataDriveFile(ConfigurationManager.AppSettings["DataDrivenFileXlsx"], "Journey1", new[] { "fromLocation", "toLocation" }, "Journey1Test");
+			}
+		}
+	}
+}
